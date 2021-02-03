62799bebc4d3220eb34ed3a1d5735dcfa012c062
Alan Dias <alan.augustodias@gmail.com>
2021-02-03 18:02:43 +0000

    Add RouteParams Decorator

--- src/startup/routes-binder.ts
+++ src/startup/routes-binder.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response, Router } from 'express';
+import {Route, RouteParam} from '../interfaces/index.js';
+import express, {Request, Response, Router} from 'express';
 
 import Controllers from '../controllers/index.js';
-import { Route } from '../interfaces/index.js';
+import {RouteParamType} from '../enum/index.js';
 
 const getKeyValue = (key: string) => (obj: Record<string, any>) => obj[key];
 
@@ -11,12 +12,42 @@ export default function RoutesBinder(): Router {
         const instance = new controller();
         const prefix = Reflect.getMetadata('prefix', controller);
         const routes: Array<Route> = Reflect.getMetadata('routes', controller);
+        const routeParams: Array<RouteParam> = Reflect.getMetadata('routeParams', controller);
 
         routes.forEach((route) => {
-            const method = getKeyValue(route.methodName)(instance);
             router[route.requestMethod](prefix + route.path, async (req: Request, res: Response) => {
-                const result = await method(req, res);
-                res.send(result);
+                const paramsOnRoute = routeParams
+                    ? routeParams.find(({methodName}) => methodName === route.methodName)
+                    : null;
+
+                paramsOnRoute?.params.sort((a, b) => {
+                    if (a.index < b.index) {
+                        return -1;
+                    }
+                    if (a.index > b.index) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                const paramsMapper = paramsOnRoute?.params?.length
+                    ? paramsOnRoute?.params.map(({type}): any => {
+                          const routeParams = {
+                              [RouteParamType.REQUEST]: req,
+                              [RouteParamType.RESPONSE]: res,
+                              [RouteParamType.BODY]: req.body,
+                              [RouteParamType.QUERY]: req.query,
+                          };
+
+                          return routeParams[type];
+                      })
+                    : [];
+
+                const result = await (getKeyValue(route.methodName)(instance) as Function).apply(
+                    Object.getPrototypeOf(instance),
+                    paramsMapper || [],
+                );
+                return res.send(result);
             });
         });
     });
